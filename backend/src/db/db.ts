76c2f94fcbaa2f97f6a65bf593f6f0927b8ce990
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2025-07-16 11:34:22 -0300

    DB setup for test

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,4 +1,7 @@
 import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
 export const db = new Client({
   connectionString: process.env.DATABASE_URL,
