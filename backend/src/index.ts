7e856636a4d8a4ecb3259b863caadc923bcf5b0e
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2025-07-16 03:09:44 -0300

    Server and index file refactored

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ const cors = require('cors');
 import testStripRoutes from './routes/testStripRoutes';
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
 
 app.use(cors());
 app.use('/api/test-strips', testStripRoutes);
@@ -12,6 +12,8 @@ app.get('/', (_req, res) => {
   res.send('Eli Backend is running!');
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Eli backend listening at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Eli backend listening at http://localhost:${PORT}`);
+// });
+
+export default app;
