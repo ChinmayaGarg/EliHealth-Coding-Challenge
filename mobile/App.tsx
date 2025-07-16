00afb00b405983b69326325aa99cb040595630a2
Chinmaya Garg <chinmayagarg@macbookpro.home>
2025-07-16 00:21:18 -0300

    Mobile Frontend boilerplate added with navigation

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CameraScreen from './src/screens/CameraScreen';
+import HistoryScreen from './src/screens/HistoryScreen';
+
+export type RootStackParamList = {
+  Camera: undefined;
+  History: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Camera">
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="History" component={HistoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
