c78e716dd19ec00bcb819859f02e17135f7f023e
Chinmaya Garg <chinmayagarg@macbookpro.home>
2025-07-16 00:12:19 -0300

    Vanilla Node Backend boilerplate added with healthcheck

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { extractQRCode } from './utils/qrReader';
+import { generateThumbnail } from './utils/thumbnail';
+import { imageSize as getImageSize } from 'image-size';
+import fs from 'fs';
+
+import { Client } from 'pg';
+
+const cors = require('cors');
+// const sizeOf = require('image-size');
+// const sizeOf = imageSize;
+
+// Setup Express
+const app = express();
+const PORT = 3000;
+
+// Enable CORS (important for frontend integration)
+app.use(cors());
+// Health check
+app.get('/', (req, res) => {
+  res.send('Eli Backend is running!');
+});
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Eli backend listening at http://localhost:${PORT}`);
+});
