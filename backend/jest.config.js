1044ee788882a7671538b29e3a21f68cc6279a00
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2025-07-16 03:09:58 -0300

    Jest Config file

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  forceExit: true,
+  verbose: true
+};
